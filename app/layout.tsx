1ce207164c0bd993279e2c8ec49c9cf530016489
Ellisvelandia <eyis619l@gmail.com>
2023-07-13 11:21:35 -0500

    movie-api

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import Header from '@/components/Header'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Next Movie App",
+  description: "themoviedb API ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
